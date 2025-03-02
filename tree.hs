bb3c2995b753e994ab8a1df1e64ed6ba5e99ab7e
Marián Tarageľ <mariantaragel@gmail.com>
2025-03-02 20:22:46 +0100

    Added more data structures for decisionTree

--- tree.hs
+++ tree.hs
@@ -7,18 +7,26 @@ data DecisionTree a b =
 data TreeSide = Root | LeftTree | RightTree
     deriving (Eq, Ord, Show, Read, Bounded, Enum)
 
+data InputLine = InputLine { tree :: DecisionTree (Int, Float) String
+                           , parent :: Int
+                           , side :: TreeSide
+                           } deriving (Show)
+
 createNode :: a -> DecisionTree a b
 createNode x = Node x EmptyTree EmptyTree
 
 createLeaf :: b -> DecisionTree a b
 createLeaf x = Leaf x
 
-insertIntoTree :: (Eq a1, Eq a2, Eq b) => DecisionTree (a1, a2) b -> DecisionTree (a1, a2) b -> Bool -> a1 -> DecisionTree (a1, a2) b
-insertIntoTree EmptyTree _ _ _ = EmptyTree
-insertIntoTree (Leaf b) _ _ _ = EmptyTree
-insertIntoTree (Node x left right) tree b n
-    | (fst x) == n = if b then (Node x left tree) else (Node x tree right)
-    | (fst x) /= n = Node x (insertIntoTree left tree b n) (insertIntoTree right tree b n)
+insertIntoTree :: DecisionTree (Int, Float) String -> InputLine -> DecisionTree (Int, Float) String
+insertIntoTree EmptyTree input =
+    if (side input) == Root
+    then tree input
+    else EmptyTree
+insertIntoTree (Leaf b) _ = Leaf b
+insertIntoTree (Node x left right) input
+    | (fst x) == (parent input) = if (side input) == RightTree then (Node x left (tree input)) else (Node x (tree input) right)
+    | (fst x) /= (parent input) = Node x (insertIntoTree left input) (insertIntoTree right input)
 
 makeTuple :: String -> (Int, DecisionTree (Int, Float) String)
 makeTuple s = (countSpaces s, treeFromString s)
@@ -34,22 +42,44 @@ dropSpaces s = drop (countSpaces s) s
 
 treeFromString :: String -> DecisionTree (Int, Float) String
 treeFromString s =
-    if (take 4 (dropSpaces s)) == "Node"
-    then createNode (read ('(' : (drop 6 (dropSpaces s)) ++ ")") :: (Int, Float))
-    else createLeaf (drop 6 (dropSpaces s))
+    if (take 4 $ dropSpaces s) == "Node"
+    then createNode $ parseNode s
+    else createLeaf $ parseLeaf s
 
 leftRight :: [Int] -> [TreeSide]
 leftRight [] = []
-leftRight (x : xs) = [Root] ++ leftRight' xs [2 .. (maximum xs)]
+leftRight (x : xs) = Root : (leftRight' xs [2 .. (maximum xs)])
 
+leftRight' :: (Eq a) => [a] -> [a] -> [TreeSide]
 leftRight' [] first = []
 leftRight' (x : xs) first =
     if x `elem` first
-    then [LeftTree] ++ leftRight' xs (deleteElem x first)
-    else [RightTree] ++ leftRight' xs (deleteElem x first)
+    then LeftTree  : (leftRight' xs $ deleteElem x first)
+    else RightTree : (leftRight' xs $ deleteElem x first)
 
 deleteElem :: (Eq a) => a -> [a] -> [a]
 deleteElem _ [] = []
 deleteElem n (x : xs)
     | x == n = deleteElem n xs
     | x /= n = x : (deleteElem n xs)
+
+findParent :: [(String, Int)] -> [Int]
+findParent [] = []
+findParent (x : y : z : xs) =
+    if (snd y) == (snd z)
+    then (fst $ parseNode $ (fst x)) : (fst $ parseNode $ (fst x)) : (findParent (z : xs)) 
+    else (fst $ parseNode $ (fst x)) : (findParent (y : z : xs))
+findParent (x : xs) = []
+
+parseNode :: String -> (Int, Float)
+parseNode s = read ('(' : (drop 6 (dropSpaces s)) ++ ")") :: (Int, Float)
+
+parseLeaf :: String -> String
+parseLeaf s = drop 6 $ dropSpaces s
+
+makeInputs :: [DecisionTree (Int, Float) String] -> [Int] -> [TreeSide] -> [InputLine]
+makeInputs [] [] [] = []
+makeInputs (a : as) (b : bs) (c : cs) = (InputLine a b c) : (makeInputs as bs cs)
+
+buildTree :: (Foldable a) => a InputLine -> DecisionTree (Int, Float) String
+buildTree inputs = foldl insertIntoTree EmptyTree inputs
